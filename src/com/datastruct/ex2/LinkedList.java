b6de7fb95a3bb5716f6e8ecbeb4db2c790609578
amos zohar <azamoszohar@gmail.com>
2023-09-05 19:05:56 +0300

    finished ex2

--- src/com/datastruct/ex2/LinkedList.java
+++ src/com/datastruct/ex2/LinkedList.java
@@ -41,6 +41,9 @@ public class LinkedList {
 	public boolean IS_WW() {
 		Node step = this.first;
 		Node doubleStep = this.first;
+		if(this.first!=null && this.first.next==null) {
+			return false;//single char
+		}
 		/* I use doubleStep to get to the middle of the list,
 		 * and if the list is odd, it will return false*/
 		while(doubleStep!=null && doubleStep.next!=null) {
@@ -64,6 +67,49 @@ public class LinkedList {
 		}
 		/*If we got here, it means that all comparisons between
 		 * the 2 halfs were true, thus we can return true*/
+		return true;
+	}
+	public boolean IS_PALINDROM() {
+		if(this.isEmpty()||this.len==1) {
+			return true;
+		}
+		if(this.len==2) {
+			return this.first.val == this.first.next.val;
+		}
+		int counter = 1;
+		Node leftEnd = this.first;
+		while(counter < this.len/2) {
+			leftEnd = leftEnd.next;
+			counter++;
+		}
+		/*If len is odd, skip middle element*/
+		if(this.len%2==1) {
+			leftEnd.next = leftEnd.next.next;
+			this.len--;
+		}
+		Node rightStart = leftEnd.next;
+		Node prev = null;
+		Node curr = rightStart;
+		while(curr!=null) {
+			Node temp = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr=temp;
+			if(curr==this.last) {
+				leftEnd.next = curr;
+				this.last = rightStart;
+			}
+		}
+		int i =0;
+		Node t1 = this.first;
+		Node t2 = leftEnd.next;
+		while(t1!=null&&t2!=null) {
+			if(t1.val!=t2.val) {
+				return false;
+			}
+			t1=t1.next;
+			t2 = t2.next;
+		}
 		return true;
 	}
 }
--- src/com/datastruct/ex2/main.java
+++ src/com/datastruct/ex2/main.java
@@ -2,20 +2,28 @@ package com.datastruct.ex2;
 
 public class main {
 	public static void main(String[] args) {
-		
-		String s1="amoszoharisawesomeamoszoharisawesome";
-		String s2 = "amoszoharisalmostawesome1amoszoharisalmostawesome";
-		String s3 = "";
-		String s4 = "A";
-		String s5 = "AA";
-		String s6= "AB";
-		String s7 = "ABA";
-		String s8 = "ABAB";
-		String s9 = "ababaababbababaaaababb";
-		String s10 = "abbabbabbaaaaabbabbabba";
-		String s11 = "baabaaabbabbabbaabaaabbabbab";
-		String s12 = "abbabbaabbabba";
-		LinkedList list = LinkedList.generateListFromString(s12);
-		System.out.println(list.IS_WW());
+		String s1="amoszoharisawesomeamoszoharisawesome";//yes ww, not pali
+		String s2 = "amoszoharisalmostawesome1amoszoharisalmostawesome";//not ww, not pali
+		String s3 = "";//yes ww, yes pali
+		String s4 = "A";//not ww, yes pali
+		String s5 = "AA";//yes ww, yes pali
+		String s6= "AB";//not ww, not pali
+		String s7 = "ABA";//not ww, yes pali
+		String s8 = "ABAB";//yes ww, not pali
+		String s9 = "ababaababbababaaaababb";//not ww, not palindrom
+		String s10 = "abbabbabbaaaaabbabbabba";//not ww, yes palindrom
+		String s11 = "baabaaabbabbabbaabaaabbabbab";//yes ww, not pali
+		String s12 = "abbabbaabbabba";//yes ww, yes pali
+		String s13 = "amossoma";//not ww, yes pali
+		String[] stringArray = {s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13};
+		for(int i =0;i<stringArray.length;i++) {
+			LinkedList list = LinkedList.generateListFromString(stringArray[i]);
+			System.out.print(i+1);
+			System.out.print(" isWW : ");
+			System.out.print(list.IS_WW());
+			System.out.print(" , is palindrom: ");
+			System.out.print(list.IS_PALINDROM());
+			System.out.println();
+		}
 	}
 }
